9a26b815ec121c05bb2114f1f18160dc8d4a217a
Ricky Rivera <contact@rickyr.dev>
2025-10-09 23:09:16 -0400

    hopefully fix 404

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import IndexPage from '@/pages/IndexPage.vue'
 import QRCode from './components/QRCode.vue'
@@ -19,7 +19,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory('/FrontendMentorChallenges/'),
   routes,
 })
 
